bb777088c725cfa962e02b1104abecd05e487fda
qibenyu <qibenyui447@yahoo.com>
2014-09-29 14:05:42 +0800

    Update continuous.py

--- script/continuous.py
+++ script/continuous.py
@@ -48,6 +48,8 @@ class CameraTest(unittest.TestCase):
         #Step 1. Launch single capture activity
         a.cmd('launch','com.intel.camera22/.Camera')
         time.sleep(2)
+        if d(text = 'Skip').wait.exists(timeout = 3000):
+            d(text = 'Skip').click.wait() 
         if  d(text = 'OK').wait.exists(timeout = 3000):
             d(text = 'OK').click.wait()
         sm.switchCaptureMode('Single')
